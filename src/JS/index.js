2b63575f0e0076b317508252d71d24aa0539e844
nataliabrunelli <154284803+nataliabrunelli@users.noreply.github.com>
2025-02-02 16:19:41 -0300

    projeto finalizado v. 01

---
+++ src/JS/index.js
@@ -0,0 +1,15 @@
+const inputs = document.querySelectorAll(".input");
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+  inputs.forEach((i) => {
+    i.classList.remove("vermelho", "verde");
+    
+    if (i.value === "") {
+      e.preventDefault();
+      i.classList.add("vermelho");
+    } else {
+      i.classList.add("verde");
+    }
+  });
+});
